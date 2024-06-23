eac9556f1e83ec44df3ca76962e202624e896db4
eli <elijahmurket@gmail.com>
2024-06-23 08:59:14 +0300

    cart adding working

--- store/urls.py
+++ store/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.store, name='store'),
     path('cart/', views.cart, name='cart'),
+    path('update_item/', views.updateItem, name='update_item'),
     path('checkout/', views.checkout, name='checkout'),
 ]
